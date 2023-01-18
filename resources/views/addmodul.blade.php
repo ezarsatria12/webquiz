469479809655c26314546cfa783df96396c12694
edgarzyah <Edgarzyah@gmail.com>
2023-01-18 22:03:26 +0700

    + addmodul page responsive

--- resources/views/addmodul.blade.php
+++ resources/views/addmodul.blade.php
@@ -5,14 +5,14 @@
     <div class="container space-y-5 m-auto px-3 sm:w-[1440px] sm:h-[1024px] sm:space-y-16">
         <div class="container h-fit space-y-5 justify-start flex flex-col m-auto sm:w-[1280px] lg:h-[64px] lg:flex-row sm:space-y-0 sm:space-x-20">
             <div name="Title" class="h-fit w-full sm:w-[630px] space-y-3">
-                <p class="">Modul Title</p>
+                <p class="">Judul Modul</p>
                 <textarea required class="outline-green-111 rounded border-2 p-2 w-full sm:w-[630px] border-gray-200 resize-none"
                 name="Title" id="Title" rows="2" placeholder="Masukkan Judul Modul"></textarea>
         </div>
     </div>
     <div class="m-auto sm:w-[1280px] h-[676px] flex flex-col justify-start space-y-5">
         <div name="Title" class="space-y-3">
-                <p class="">Modul Description</p>
+                <p class="">Deskripsi Modul</p>
                 <textarea class="w-full sm:w-[630px] outline-green-111 rounded border-2 p-2 border-gray-200 resize-none" 
                 name="Title" id="Title" cols="80" rows="6" placeholder="Masukkan Judul Modul"></textarea>
         </div>

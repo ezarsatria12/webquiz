ae5c41204fe1cf352a945b0e7dea467c77e573b9
Ezarsatria12 <Ezar3004>
2023-01-18 15:21:00 +0700

    addmodule

--- app/Http/Controllers/ModuleController.php
+++ app/Http/Controllers/ModuleController.php
@@ -13,7 +13,7 @@ class ModuleController extends Controller
      */
     public function index()
     {
-        //
+        return view('admin.module');
     }
 
     /**
@@ -23,7 +23,7 @@ class ModuleController extends Controller
      */
     public function create()
     {
-        //
+        
     }
 
     /**

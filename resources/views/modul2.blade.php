8f2026170a86ff5ccde327f834c8fdd456011de0
edgarzyah <Edgarzyah@gmail.com>
2023-01-19 14:37:11 +0700

    modul adjustment

--- resources/views/modul2.blade.php
+++ resources/views/modul2.blade.php
@@ -12,118 +12,57 @@
     </div>
 </div>
     <div class=" h-fit flex flex-row space-x-[32px] sm:text-[20px] sm:w-[1280px] ">
-            <a class="hover:text-green-111 focus:text-green-111" href="#    ">Terbaru</a>
+            <a class="hover:text-green-111 focus:text-green-111" href="#">Terbaru</a>
             <a class="hover:text-green-111 focus:text-green-111" href="#">Puisi</a>
             <a class="hover:text-green-111 focus:text-green-111" href="#">Pantun</a>
             </div>
             <!-- cardsection -->
-            <div class="container flex flex-col sm:flex-row h-fit pb-[10px] space-y-5 sm:w-[1280px] sm:space-x-3 sm:flex-wrap sm:space-y-5">
+            <div class="container mx-auto flex flex-wrap sm:flex-row space-x-2 w-[360px] h-fit pb-[10px] space-y-3 sm:w-[1280px] sm:flex-wrap sm:space-y-5">
             <!-- card1 -->
             <a href="#">
-            <div class="container flex flex-col justify-start w-full h-[383px] sm:w-[400px] sm:h-[430px] shadow-boxjawaban rounded-[10px] bg-white">
+            <div class="container w-[150px] m-auto flex flex-col justify-start h-fit sm:w-[400px] sm:h-[430px] shadow-card rounded-[10px] bg-white">
                     <div>
-                        <img class="mx-auto my-[15px] rounded-[10px] sm:w-[350px] sm:h-[185px]  "
+                    <img class="my-[15px] rounded-[10px] px-[10px] sm:w-[350px] sm:h-[185px]  "
                     src="pictures/template.png" alt="thumbnail-modul">
-                    <h1 class="text-3xl flex justify-start px-7">Judul Modul</h1>
-                    <p class="flex flex-col justify-start px-7">deskripsi Modul</p>
+                    <h1 class="flex justify-start px-3 sm:px-7 text-[16px] sm:text-3xl ">Judul Modul</h1>
+                    <p class="text-[12px] sm:text-[20px] flex flex-col  justify-start px-3 sm:px-7">deskripsi Modul</p>
                     </div>
-                    <div class="container justify-end w-full h-auto flex pr-5 mb-5 mt-auto flex-row">
-                    <img class="w-[50px] h-[50px]" src="pictures/go.png" alt="">
+                    <div class="container justify-end w-full h-auto flex pr-3 sm:pr-5 mb-3 sm:mb-5 mt-auto flex-row">
+                    <img class="w-[16px] h-[16px] sm:w-[50px] sm:h-[50px]" src="pictures/go.png" alt="">
                 </a>
                 </div>
             </div>
             <!-- card end -->
             <!-- card1 -->
             <a href="#">
-            <div class="container flex flex-col justify-start w-full h-[383px] sm:w-[400px] sm:h-[430px] shadow-boxjawaban rounded-[10px] bg-white">
+            <div class="container w-[150px] m-auto flex flex-col justify-start h-fit sm:w-[400px] sm:h-[430px] shadow-card rounded-[10px] bg-white">
                     <div>
-                        <img class="mx-auto my-[15px] rounded-[10px] sm:w-[350px] sm:h-[185px]  "
+                    <img class="my-[15px] rounded-[10px] px-[10px] sm:w-[350px] sm:h-[185px]  "
                     src="pictures/template.png" alt="thumbnail-modul">
-                    <h1 class="text-3xl flex justify-start px-7">Judul Modul</h1>
-                    <p class="flex flex-col justify-start px-7">deskripsi Modul</p>
+                    <h1 class="flex justify-start px-3 sm:px-7 text-[16px] sm:text-3xl ">Judul Modul</h1>
+                    <p class="text-[12px] sm:text-[20px] flex flex-col  justify-start px-3 sm:px-7">deskripsi Modul</p>
                     </div>
-                    <div class="container justify-end w-full h-auto flex pr-5 mb-5 mt-auto flex-row">
-                    <img class="w-[50px] h-[50px]" src="pictures/go.png" alt="">
+                    <div class="container justify-end w-full h-auto flex pr-3 sm:pr-5 mb-3 sm:mb-5 mt-auto flex-row">
+                    <img class="w-[16px] h-[16px] sm:w-[50px] sm:h-[50px]" src="pictures/go.png" alt="">
                 </a>
                 </div>
             </div>
             <!-- card end -->
             <!-- card1 -->
             <a href="#">
-            <div class="container flex flex-col justify-start w-full h-[383px] sm:w-[400px] sm:h-[430px] shadow-boxjawaban rounded-[10px] bg-white">
+            <div class="container w-[150px] m-auto flex flex-col justify-start h-fit sm:w-[400px] sm:h-[430px] shadow-card rounded-[10px] bg-white">
                     <div>
-                        <img class="mx-auto my-[15px] rounded-[10px] sm:w-[350px] sm:h-[185px]  "
+                    <img class="my-[15px] rounded-[10px] px-[10px] sm:w-[350px] sm:h-[185px]  "
                     src="pictures/template.png" alt="thumbnail-modul">
-                    <h1 class="text-3xl flex justify-start px-7">Judul Modul</h1>
-                    <p class="flex flex-col justify-start px-7">deskripsi Modul</p>
+                    <h1 class="flex justify-start px-3 sm:px-7 text-[16px] sm:text-3xl ">Judul Modul</h1>
+                    <p class="text-[12px] sm:text-[20px] flex flex-col  justify-start px-3 sm:px-7">deskripsi Modul</p>
                     </div>
-                    <div class="container justify-end w-full h-auto flex pr-5 mb-5 mt-auto flex-row">
-                    <img class="w-[50px] h-[50px]" src="pictures/go.png" alt="">
+                    <div class="container justify-end w-full h-auto flex pr-3 sm:pr-5 mb-3 sm:mb-5 mt-auto flex-row">
+                    <img class="w-[16px] h-[16px] sm:w-[50px] sm:h-[50px]" src="pictures/go.png" alt="">
                 </a>
                 </div>
             </div>
             <!-- card end -->
-            <!-- card1 -->
-            <a href="#">
-            <div class="container flex flex-col justify-start w-full h-[383px] sm:w-[400px] sm:h-[430px] shadow-boxjawaban rounded-[10px] bg-white">
-                    <div>
-                        <img class="mx-auto my-[15px] rounded-[10px] sm:w-[350px] sm:h-[185px]  "
-                    src="pictures/template.png" alt="thumbnail-modul">
-                    <h1 class="text-3xl flex justify-start px-7">Judul Modul</h1>
-                    <p class="flex flex-col justify-start px-7">deskripsi Modul</p>
-                    </div>
-                    <div class="container justify-end w-full h-auto flex pr-5 mb-5 mt-auto flex-row">
-                    <img class="w-[50px] h-[50px]" src="pictures/go.png" alt="">
-                </a>
-                </div>
-            </div>
-            <!-- card end -->
-            <!-- card1 -->
-            <a href="#">
-            <div class="container flex flex-col justify-start w-full h-[383px] sm:w-[400px] sm:h-[430px] shadow-boxjawaban rounded-[10px] bg-white">
-                    <div>
-                        <img class="mx-auto my-[15px] rounded-[10px] sm:w-[350px] sm:h-[185px]  "
-                    src="pictures/template.png" alt="thumbnail-modul">
-                    <h1 class="text-3xl flex justify-start px-7">Judul Modul</h1>
-                    <p class="flex flex-col justify-start px-7">deskripsi Modul</p>
-                    </div>
-                    <div class="container justify-end w-full h-auto flex pr-5 mb-5 mt-auto flex-row">
-                    <img class="w-[50px] h-[50px]" src="pictures/go.png" alt="">
-                </a>
-                </div>
-            </div>
-            <!-- card end -->
-            <!-- card1 -->
-            <a href="#">
-            <div class="container flex flex-col justify-start w-full h-[383px] sm:w-[400px] sm:h-[430px] shadow-boxjawaban rounded-[10px] bg-white">
-                    <div>
-                        <img class="mx-auto my-[15px] rounded-[10px] sm:w-[350px] sm:h-[185px]  "
-                    src="pictures/template.png" alt="thumbnail-modul">
-                    <h1 class="text-3xl flex justify-start px-7">Judul Modul</h1>
-                    <p class="flex flex-col justify-start px-7">deskripsi Modul</p>
-                    </div>
-                    <div class="container justify-end w-full h-auto flex pr-5 mb-5 mt-auto flex-row">
-                    <img class="w-[50px] h-[50px]" src="pictures/go.png" alt="">
-                </a>
-                </div>
-            </div>
-            <!-- card end -->
-            <!-- card1 -->
-            <a href="#">
-            <div class="container flex flex-col justify-start w-full h-[383px] sm:w-[400px] sm:h-[430px] shadow-boxjawaban rounded-[10px] bg-white">
-                    <div>
-                        <img class="mx-auto my-[15px] rounded-[10px] sm:w-[350px] sm:h-[185px]  "
-                    src="pictures/template.png" alt="thumbnail-modul">
-                    <h1 class="text-3xl flex justify-start px-7">Judul Modul</h1>
-                    <p class="flex flex-col justify-start px-7">deskripsi Modul</p>
-                    </div>
-                    <div class="container justify-end w-full h-auto flex pr-5 mb-5 mt-auto flex-row">
-                    <img class="w-[50px] h-[50px]" src="pictures/go.png" alt="">
-                </a>
-                </div>
-            </div>
-            <!-- card end -->
-            
         </div>
     </div>
 

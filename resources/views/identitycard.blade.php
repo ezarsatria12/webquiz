6845613b055f8a9e5e49385b05d30e0ae495584c
edgarzyah <Edgarzyah@gmail.com>
2023-01-21 17:59:30 +0700

    identitas card

--- resources/views/identitycard.blade.php
+++ resources/views/identitycard.blade.php
@@ -1,10 +1,10 @@
 <!doctype html>
 <html>
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  @vite('resources/css/app.css')
-  <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/modul.css')}}">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+@vite('resources/css/app.css')
+<link rel="stylesheet" type="text/css" href="{{ URL::asset('css/modul.css')}}">
 </head>
 <body>
 <div class="justify-center flex m-auto my-[150px] w-full h-full">

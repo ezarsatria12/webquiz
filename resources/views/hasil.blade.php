784dc6d86630e3533b1c795b08ce483e009e0c64
edgarzyah <Edgarzyah@gmail.com>
2023-01-24 11:46:26 +0700

    + aditional page

---
+++ resources/views/hasil.blade.php
@@ -0,0 +1,11 @@
+@extends('partials.index')
+
+@section('container')
+<section>
+    <div class="container flex flex-col mt-[48px] gap-[48px]  m-auto px-3 sm:px-0 sm:w-[1280px] h-full">
+    <div class="container flex flex-col">
+        <h1 class="text-4xl">Hasil</h1>
+    </div>
+</div>
+</section>
+@endsection

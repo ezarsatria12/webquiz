3123bfc8ed91f3d607f1f8c02df9860d575489b4
edgarzyah <Edgarzyah@gmail.com>
2023-02-03 00:38:44 +0700

    essay finish

--- resources/views/essay.blade.php
+++ resources/views/essay.blade.php
@@ -9,10 +9,13 @@
     @vite('resources/css/app.css')
 </head>
 
-<body class="container sm:w-[1440px] sm:h-[1024px] m-auto">
+<body class="container bg-black sm:bg-gray-800 sm:w-[1440px] sm:h-[1024px] m-auto">
+    <div class="my-auto self-center text-2xl text-center sm:hidden block">
+       <p class="m-auto text-white text-center pt-72 pb-5">Please rotate your phone</p>
+    <img src="{{ URL('pictures/rotate.gif') }}" class="m-auto w-[200px] h-[200px]" alt=""></div>
     <form action="{{route('showesayvalid',[$quiz, $student,$esay])}}" method="POST"">
         @csrf
-    <div class=" container h-fit sm:w-[1280px] sm:h-[960px] px-3 pt-[32px] flex flex-col space-y-[20px]">
+    <div class="hidden sm:block container h-fit sm:w-[1280px] sm:h-[960px] px-3 pt-[32px] flex-col space-y-[20px]">
         <div class="w-[50px] h-[50px]">
             <a href="#back">
                 <img src="pictures/back button.png" alt="">
@@ -40,12 +43,12 @@
                 src="{{asset('storage/'.$esays->media) }}" alt="asd" type="image">
             @endif
             @endif
-            <h1 class="text-2xl flex sm:text-4xl sm:m-[10px] sm:w-full ">{{$esays->question}}
+            <h1 class="container bg-gray-300 p-5 rounded-xl text-2xl flex sm:text-4xl h-[400px] sm:m-[10px] sm:w-full ">{{$esays->question}}
             </h1>
         </div>
         <!-- button jawaban -->
         <div class="container sm:w-[1280px] sm:h-[320px] flex flex-col space-y-[20px]">
-            <textarea name="answer" class="bg-gray-100 h-full w-full rounded-[10px] form-control block px-3 py-1.5 text-2xl font-normal text-gray-700
+            <textarea name="answer" class="bg-gray-100 h-full w-full rounded-[10px] form-control block px-1 py-5 text-2xl font-normal text-gray-700
         bg-clip-padding border border-solid outline outline-gray-400 transition ease-in-outm-0
         focus:text-gray-700 focus:bg-white focus:outline-green-111 focus:outline-4" id="exampleFormControlTextarea1"
                 rows="4" placeholder="Masukkan Jawaban Disini">
@@ -53,10 +56,9 @@
         </div>
         <!-- next/prev button -->
         <div class="container justify-end w-full sm:w-[1280px] sm:h-[104px] flex flex-row">
-            <button
-                class="hover:brightness-110 hover:scale-105 justify-center w-[60px] h-[60px] sm:w-[80px] sm:h-[80px]"
-                type="submit">
-                <img src="{{ URL('pictures/next.png') }}" alt="">
+            <button href="#" class="inline-block px-5 py-1 hover:scale-105 font-inter text-2xl text-center text-white 
+                    bg-green-111 rounded-md focus:shadow-card" type="submit">
+                            Submit</button>
             </button>
         </div>
         </div>

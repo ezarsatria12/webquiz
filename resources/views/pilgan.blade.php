b4157508fe0d5ab2651725a8f0b795324bd739f4
edgarzyah <Edgarzyah@gmail.com>
2023-02-03 00:25:54 +0700

    random color option

--- resources/views/pilgan.blade.php
+++ resources/views/pilgan.blade.php
@@ -9,9 +9,12 @@
     @vite('resources/css/app.css')
 </head>
 
-<body class="container sm:w-[1440px] sm:h-[1024px] m-auto">
+<body class="container bg-black sm:bg-gray-800 sm:w-[1440px] sm:h-[1024px] m-auto">
+    <div class="my-auto self-center text-2xl text-center sm:hidden block">
+       <p class="m-auto text-white text-center pt-72 pb-5">Please rotate your phone</p>
+    <img src="{{ URL('pictures/rotate.gif') }}" class="m-auto w-[200px] h-[200px]" alt=""></div>
     <!-- pilgan -->
-    <div class="container h-fit sm:w-[1280px] sm:h-[960px] px-3 pt-[32px] flex flex-col space-y-[50px]">
+    <div class="hidden sm:block container h-fit sm:w-[1280px] sm:h-[960px] px-3 pt-[32px] flex-col space-y-[50px]">
         <div class="w-[50px] h-[50px]">
             <a href="#back">
                 <img src="pictures/back button.png" alt="">
@@ -37,7 +40,7 @@
                 src="{{asset('storage/'.$pilgans->media) }}" alt="asd" type="image">
             @endif
             @endif
-            <h1 class="text-2xl flex sm:text-4xl sm:m-[10px] sm:w-full ">{{$pilgans->question}}
+            <h1 class="container bg-gray-300 p-5 rounded-xl text-2xl flex sm:text-4xl h-[400px] sm:m-[10px] sm:w-full ">{{$pilgans->question}}
             </h1>
         </div>
         <!-- button jawaban -->
@@ -47,28 +50,30 @@
                 @csrf
                 <!-- opsi benar -->
                 @foreach ($pilgans->mutichoisechoise as $opsi)
-                <button for="opsi-1" name="pilgan" value="{{ $opsi->id }}"
-                    class="flex focus:brightness-75 hover:scale-110 hover:brightness-75 bg-red-redFF6767 py-[20px] text-start shadow-boxjawaban sm:h-[300px] h-[150px] w-[150px] sm:w-[300px] rounded focus:text-black ml-2 text-sm font-medium text-gray-900 ">
+                <button id="box" onload="getRandomColor()" for="opsi-1" name="pilgan" value="{{ $opsi->id }}"
+                    class="flex focus:brightness-75 hover:scale-110 hover:brightness-75 py-[20px] text-start shadow-boxjawaban
+                    sm:h-[300px] h-[150px] w-[150px] sm:w-[300px] rounded focus:text-black ml-2 text-sm font-medium text-gray-900 ">
                     <span class="text-2xl m-auto focus:brightness-50">{{$opsi->answer}}</span>
                     <input class="hidden" id="opsi-1" type="radio" value="10" name="pilgan" value="{{ $opsi->id }}">
                     <br>
                 </button>
+                <script>
+                    function getRandomColor() {
+                        var letters = "0123456789ABCDEF";
+                        var color = "#";
+                        for (var i = 0; i < 6; i++) {
+                        color += letters[Math.floor(Math.random() * 16)];
+                        }
+                        return color;
+                    }
+
+                    var container = document.getElementById("box");
+                    container.style.backgroundColor = getRandomColor();
+                </script>
                 @endforeach
 
             </div>
             <!-- next/prev button -->
-            <div class="container justify-end w-[335px] sm:w-[1280px] sm:h-[104px] flex flex-row">
-                <a class="hover:brightness-110 hover:scale-105 justify-center mx-[20px] w-[60px] h-[60px] sm:w-[80px] sm:h-[80px]"
-                    href="#prev">
-                    <img src="{{ URL('pictures/prev.png') }}" alt="">
-                </a>
-                <a class="hover:brightness-110 hover:scale-105 justify-center w-[60px] h-[60px] sm:w-[80px] sm:h-[80px]"
-                    href="#next">
-                    <button class="submit-button" type="submit">
-                        <img src="{{ URL('pictures/next.png') }}" alt="">
-                    </button>
-                </a>
-            </div>
         </form>
         @if(session('success'))
         <script>

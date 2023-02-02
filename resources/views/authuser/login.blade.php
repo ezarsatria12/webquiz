b810efc7892ada15956ad291e821e96954e8f2e7
edgarzyah <Edgarzyah@gmail.com>
2023-02-02 23:27:50 +0700

    login register page finish

--- resources/views/authuser/login.blade.php
+++ resources/views/authuser/login.blade.php
@@ -9,11 +9,12 @@
 </head>
 
 <body>
-    <div class="justify-center flex m-auto my-[150px] w-full h-full">
-        <div class="bg-white shadow-md border border-gray-200 rounded-lg sm:w-[491px] sm:h-[654px] p-4 sm:p-6 lg:p-8">
-            <form class="space-y-2 sm:space-y-6" action="{{route('authenticate')}}" method="POST">
+    <div class="justify-center flex  mx-5 my-[150px] h-full">
+        <div class="bg-white shadow-md border border-gray-200 rounded-lg w-full sm:w-[491px] h-fit p-4 sm:p-6">
+            <form class="space-y-2 sm:space-y-6" action="{{route('authenticate')}}" method="POST"
+                onsubmit="return validatePassword()">
                 @csrf
-                <h3 class="pb-[50px] text-xl font-medium text-center text-gray-900">Sign in</h3>
+                <h3 class="pb-[20px] text-xl sm:text-3xl font-medium text-center text-gray-900">Sign in</h3>
                 <div>
                     <!-- username -->
                     <label for="email" class="text-xs sm:text-sm font-medium text-gray-900 block mb-2">Your
@@ -21,7 +22,7 @@
                     <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300
         text-gray-900 text-xs sm:text-sm rounded-lg focus:ring-green-111 
         outline-green-111 focus:border-ring-green-111 block
-        w-full p-2.5" placeholder="Username" required="">
+        w-full p-2.5" placeholder="Example@email.com" required="">
                 </div>
                 <div class="space-y-[8px]">
                     <!-- pass -->
@@ -31,6 +32,16 @@
                         class="bg-gray-50 border outline-green-111
           border-gray-300 text-gray-900 text-xs sm:text-sm rounded-lg focus:ring-green-111 focus:border-green-111 block w-full p-2.5" required="">
                     <input type="checkbox" onclick="myFunction()" class="my-auto"> Show Password
+                    <script>
+                    function validatePassword() {
+                        var password = document.getElementById("password").value;
+                        if (password.length < 8) {
+                            alert("Password must be at least 8 characters long");
+                            return false;
+                        }
+                        return true;
+                    }
+                    </script>
                     <!--  -->
                     <script>
                     function myFunction() {
@@ -44,22 +55,23 @@
                     </script>
                     <br><br>
                     <!--  -->
-                    <a href="#" class="text-sm text-blue-700 hover:underline ">Lost Password?</a>
+                    <a href="#" class="hidden text-sm text-blue-700 hover:underline ">Lost Password?</a>
                     <!-- button -->
                     <button type="submit" class="w-full text-white bg-green-111 hover:brightness-75
             focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5
             text-center">Login to your account</button>
                     <br>
-                    Not registered? <a href="/register"
-                        class=" text-sm font-medium text-blue-700 hover:underline">Create account</a>
+                    Not registered?
+                    <a href="/register" class=" text-sm font-medium text-blue-700 hover:underline">Create account</a>
                 </div>
             </form>
+            <p class="text-center text-xl py-2">or</p>
             <form action="/auth/google/redirect">
-                <button type="submit" class="w-full text-black bg-white hover:brightness-75
+                <button type="submit" class="w-full border text-black bg-white hover:brightness-75
                             focus:ring-4 font-medium text-2xl rounded-lg px-5 py-2
                             text-center flex flex-row">
-                    <img src="{{ URL('pictures/google.png') }}" class="w-10 h-10" alt="">
-                    <p class="text-center w-full">Login With Google</p>
+                    <img src="{{ URL('pictures/google.png') }}" class="w-6 h-6 sm:w-10 sm:h-10 m-auto" alt="">
+                    <p class="text-center w-full m-auto text-sm sm:text-2xl">Login With Google</p>
                 </button>
             </form>
         </div>
--- resources/views/authuser/register.blade.php
+++ resources/views/authuser/register.blade.php
@@ -12,8 +12,9 @@
     <div class="justify-center flex m-auto my-[150px] w-full h-full">
         <div
             class="bg-white shadow-md mx-[20px] border w-full border-gray-200 rounded-lg sm:w-[491px] h-fit p-4 sm:p-6 lg:p-8">
-            <form class="space-y-2 sm:space-y-4" action="/register" method="POST">
-                <h3 class="pb-[30px] text-xl font-medium text-center text-gray-900">Register</h3>
+            <form class="space-y-2 sm:space-y-4" action="/register" method="POST" onsubmit="return validatePassword()">
+                @csrf
+                <h3 class="pb-[20px] text-xl sm:text-3xl font-medium text-center text-gray-900">Register</h3>
                 <div>
                     <!-- username -->
                     <label for="username" class="text-xs sm:text-sm font-medium text-gray-900 block mb-2">Your
@@ -43,7 +44,17 @@
                         <input type="checkbox" onclick="myFunction()" class="my-auto">
                         <p class="px-3">Show Password</p>
                     </div>
-
+                    <!-- validate pass min 8 char -->
+                    <script>
+                    function validatePassword() {
+                        var password = document.getElementById("password").value;
+                        if (password.length < 8) {
+                            alert("Password must be at least 8 characters long");
+                            return false;
+                        }
+                        return true;
+                    }
+                    </script>
                     <!--  -->
                     <script>
                     function myFunction() {
@@ -57,18 +68,20 @@
                     </script>
                     <br>
                     <!-- button -->
-                    <button type="submit" class="w-full text-white bg-green-111 hover:brightness-75
+                    <button type="submit" value="Submit" class="w-full text-white bg-green-111 hover:brightness-75
                         focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5
                         text-center">Create account</button>
                     <br>
+                    <a href="/login" class=" text-sm font-medium text-blue-700 hover:underline">Already have
+                        account?</a>
             </form>
             <p class="text-center">or</p>
             <form action="/auth/google/redirect">
-                <button type="submit" class="w-full text-black bg-white hover:brightness-75
-                        focus:ring-4 font-medium text-2xl rounded-lg px-5 py-2
-                        text-center flex flex-row">
-                    <img src="{{ URL('pictures/google.png') }}" class="w-10 h-10" alt="">
-                    <p class="text-center w-full">Login With Google</p>
+                <button type="submit" class="w-full border text-black bg-white hover:brightness-75
+                            focus:ring-4 font-medium text-2xl rounded-lg px-5 py-2
+                            text-center flex flex-row">
+                    <img src="{{ URL('pictures/google.png') }}" class="w-6 h-6 sm:w-10 sm:h-10 m-auto" alt="">
+                    <p class="text-center w-full m-auto text-sm sm:text-2xl">Login With Google</p>
                 </button>
             </form>
         </div>
